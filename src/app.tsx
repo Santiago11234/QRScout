df90feb62f82bf5b09e72397612cfb10784a082a
Santiago11234 <89491348+Santiago11234@users.noreply.github.com>
2024-03-22 07:29:19 -0500

    fixed navbar

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,7 @@ import { useQRScoutState } from './store/store';
 
 // import { addSampleTeam } from "../outerConfig/routes";
 
-import  DataMain  from "./displayDataComponents/dataMain";
+import DataMain from './displayDataComponents/dataMain';
 
 export function App() {
   const formData = useQRScoutState(state => state.formData);
@@ -25,15 +25,16 @@ export function App() {
     <div className="min-h-screen py-2 dark:bg-gray-700">
       <Header />
       <main className="flex flex-1 flex-col items-center justify-center px-4 text-center">
-        <div className="w-full my-4 grid grid-cols-4 gap-4">
-          <h1 className="font-sans text-6xl font-bold col-span-3 text-center">
+
+        <div className="w-full my-4 flex flex-col md:flex-row sm:flex-row justify-between items-center">
+          <h1 className="font-sans text-6xl font-bold text-center w-full">
             <div className={`font-rhr text-red-rhr`}>{formData.page_title}</div>
           </h1>
 
-          <div className="col-span-1 justify-between items-end">
+          <div className="flex flex-col md:flex-row justify-between items-center w-full">
             <a
               href="#"
-              className="text-red-rhr font-bold py-2 px-4 rounded hover:bg-red-rhr hover:text-white transition-colors duration-200"
+              className="text-red-rhr font-bold py-2 px-4 rounded hover:bg-red-rhr hover:text-white transition-colors duration-200 w-full md:w-auto"
               onClick={e => {
                 e.preventDefault();
                 setScouting(true);
@@ -43,7 +44,7 @@ export function App() {
             </a>
             <a
               href="#"
-              className="text-red-rhr font-bold py-2 px-4 rounded hover:bg-red-rhr hover:text-white transition-colors duration-200"
+              className="text-red-rhr font-bold py-2 px-4 rounded hover:bg-red-rhr hover:text-white transition-colors duration-200 w-full md:w-auto"
               onClick={e => {
                 e.preventDefault();
                 setScouting(false);
@@ -68,7 +69,7 @@ export function App() {
           </>
         ) : (
           <>
-            <DataMain/>
+            <DataMain />
           </>
         )}
       </main>
